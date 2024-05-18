eb91cafb72a969c83198efa06d333f464dce9ca0
Gabriel silveira <gsilveira551@gmail.com>
2024-05-18 12:24:05 -0300

    projeto spring

---
+++ ProjetoSpring/simple/src/test/java/projetospring/simple/SimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package projetospring.simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
